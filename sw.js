564de394e9eba03343e3c492106acafc9caa9c29
Bruninho64 <killerboys94400@gmail.com>
2026-08-11 19:15:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-const C='mm1';
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(C).then(c=>c.addAll([
-      './',
-      'index.html',
-      'manifest.json',
-      'icône-192.png',
-      'icône-512.png'
-    ]))
-  );
-});
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
+const C='my-mercedes-v4';const A=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
