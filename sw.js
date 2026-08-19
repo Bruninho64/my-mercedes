b29317de1da3f239e5d6bb5fd908c9d8f97daaa1
Bruninho64 <killerboys94400@gmail.com>
2026-08-19 17:13:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
-const CACHE='my-mercedes-v6-local-backup';
+const CACHE='my-mercedes-v6-corrigee-20260819-1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    ])
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  event.respondWith(
+    fetch(req).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(cache=>cache.put(req,copy));
+      return resp;
+    }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+  );
+});
